530f5074394d1df15ba8761a6edf21b379fa252b
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-06-11 23:07:38 -0400

    Updates! (BACKUP)

--- Include/discordcoreloader/SSLClients.hpp
+++ Include/discordcoreloader/SSLClients.hpp
@@ -297,8 +297,6 @@ namespace DiscordCoreLoader {
 
 		std::string& getInputBuffer() noexcept;
 
-		bool checkIfConnected() noexcept;
-
 		uint64_t getBytesRead() noexcept;
 
 		~WebSocketSSLShard();
