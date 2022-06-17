3f7f812bc86fa37a784b58aaf472b07e6e856c5a
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-06-17 19:52:11 -0400

    Updates! (Implemented a generator-agent-thread)

--- Include/discordcoreloader/ObjectGenerator.hpp
+++ Include/discordcoreloader/ObjectGenerator.hpp
@@ -29,17 +29,17 @@ namespace DiscordCoreLoader {
 
 	class ObjectGenerator : public Randomizer {
 	  public:
-		std::unique_ptr<GuildMemberData> generateGuildMember(const std::string& guildId, const std::vector<std::unique_ptr<RoleData>>& roles);
+		GuildMemberData generateGuildMember(const std::string& guildId, const std::vector<RoleData>& roles);
 
 		UnavailableGuild generateUnavailableGuild(uint64_t minValue, uint64_t maxValue);
 
-		std::unique_ptr<ChannelData> generateChannel(std::string guildId);
+		ChannelData generateChannel(std::string guildId);
 
-		std::unique_ptr<GuildData> generateGuild(std::string guildId);
+		GuildData generateGuild(std::string guildId);
 
-		std::unique_ptr<RoleData> generateRole(uint64_t position);
+		RoleData generateRole(uint64_t position);
 
-		std::unique_ptr<UserData> generateUser();
+		UserData generateUser();
 
 	  protected:
 		uint64_t stdDeviationForStringLength{};
